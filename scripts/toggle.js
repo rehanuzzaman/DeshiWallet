dbd4737b197e11e28e9b5ec517814effa170ed98
Rehanuz Zaman <71603107+rehanuzzaman@users.noreply.github.com>
2025-04-05 06:51:20 +0000

    Cash Out Form Added

---
+++ scripts/toggle.js
@@ -0,0 +1,12 @@
+document.getElementById('addmoney').style.display = 'none';
+document.getElementById('cashout').style.display = 'none';
+
+document.getElementById('add-money-box').addEventListener('click', function(){
+    document.getElementById('cashout').style.display = 'none';
+    document.getElementById('addmoney').style.display = 'block';
+})
+
+document.getElementById('cash-out-box').addEventListener('click', function(){
+    document.getElementById('addmoney').style.display = 'none';
+    document.getElementById('cashout').style.display = 'block';
+})
